55c0bf227babf18c606bcd4d5a2557e27fc799d9
sivalalith <kspvsivalalith@gmail.com>
2024-07-18 17:43:54 +0530

    added routes to our app

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
+import { createBrowserRouter, RouterProvider } from "react-router-dom"; // to configure routes for our app
 
 /**
  * Header (Navbar)
@@ -36,8 +37,24 @@ const AppLayout = () => {
   );
 };
 
+// the configuration for routing
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
+
 // root element set -> from here React works (like an entry point to the UI)
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// render the app layout on to the screen
-root.render(<AppLayout />);
+// render a layout on to the screen according to the route
+root.render(<RouterProvider router={router} />);
